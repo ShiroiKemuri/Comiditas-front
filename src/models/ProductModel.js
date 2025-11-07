5441fe6b5e9058f31a754244574b2763cca5bbd2
Alvaro-DEV555 <alvarolucumi40@gmail.com>
2025-11-06 19:34:17 -0500

    V1-CarritoModal

---
+++ src/models/ProductModel.js
@@ -0,0 +1,9 @@
+export class ProductModel {
+  constructor(id, name, price, description, imageUrl) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+  }
+}
