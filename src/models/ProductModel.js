7542496d957f09658176d38e081a66a54d452755
The-Pija <jhon.perlaza@correounivalle.edu.co>
2025-11-06 21:40:22 -0500

    Revert "Decime que si"

--- src/models/ProductModel.js
+++
@@ -1,9 +0,0 @@
-export class ProductModel {
-  constructor(id, name, price, description, imageUrl) {
-    this.id = id;
-    this.name = name;
-    this.price = price;
-    this.description = description;
-    this.imageUrl = imageUrl;
-  }
-}
--- src/router/index.js
+++ src/router/index.js
@@ -1,63 +1,37 @@
-import { createRouter, createWebHistory } from "vue-router";
 
-// Vistas de cliente
-import HomeScreen from "@/views/HomeScreen.vue";
-import CartView from "@/views/CartView.vue";
-
-// Vistas de administrador
-import AdminView from "@/views/adminCreate.vue";
-import AdminDeleteView from "@/views/adminDelete.vue";
-import AdminUpdateView from "@/views/adminUpdate.vue";
-import AdminSearch from "@/views/adminSearch.vue";
-import FinalizarCompra from "@/views/FinalizarCompra.vue";
-
+import { createRouter, createWebHistory } from 'vue-router'
+import AdminView from '@/views/adminCreate.vue'
+import AdminDeleteView from '@/views/adminDelete.vue'
+import AdminUpdateView from '@/views/adminUpdate.vue'
+import AdminSearch from '@/views/adminSearch.vue'
 const routes = [
-  // 👇 Ruta principal (cliente)
   {
-    path: "/",
-    name: "Home",
-    component: HomeScreen,
-  },
-
-  // 👇 Carrito de compras
-  {
-    path: "/cart",
-    name: "Cart",
-    component: CartView,
-  },
-
-  // 👇 Módulo de administración
-  {
-    path: "/admin/create",
-    name: "AdminCreate",
-    component: AdminView,
+    path: '/admin/create',
+    name: 'AdminCreate',
+    component: AdminView
   },
   {
-    path: "/admin/delete",
-    name: "AdminDelete",
-    component: AdminDeleteView,
+    path: '/admin/delete',
+    name: 'AdminDelete',
+    component: AdminDeleteView
   },
   {
-    path: "/admin/update",
-    name: "AdminUpdate",
-    component: AdminUpdateView,
   },
   {
-    path: "/admin/search",
-    name: "AdminSearch",
-    component: AdminSearch,
+    path: '/admin/update',
+    name: 'AdminUpdate',
+    component: AdminUpdateView
   },
-
   {
-    path: "/finalizar/compra",
-    name: "FinalizarCompra",
-    component: FinalizarCompra,
-  },
-];
+    path: '/admin/search',
+    name: 'AdminSearch',
+    component: AdminSearch
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes,
-});
+  routes
+})
 
-export default router;
+export default router
